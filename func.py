74c671f2792f91661a5b9a798654da06f52d36b2
wlw <1468087639@qq.com>
2022-02-21 20:46:13 +0800

    final version

--- func.py
+++ func.py
@@ -4,14 +4,11 @@ import itertools as it
 import data
 from data import KeyGroup, LetterGroup, MIN_CYCLE_NUM
 
-
 global min_w, min_f
 min_f = data.f_init
-min_w = float(6963200.304931576)
-
 
 ''' 
-    file I/O
+    文件I/O操作
 '''
 def GetMatrix(file_path):
     fp = open(file_path, "r")
@@ -22,44 +19,57 @@ def OutputMatrix(file_path, M):
     fp.write(str(M))
     fp.close()
 
-
-'''
-    Major functions
-'''
-# only runs once to generate matrix p, 
-def GenMatrixP():
-    L = GetMatrix("Matrix_L.txt")
-    C = GetMatrix("Matrix_D.txt")
-    P = [[0 for i in range(0, 26)] for j in range(0, 26)]
-    for i in range(0,26):
-        for j in range(0, 26):
-            P[i][j]= np.vdot(L[i][j], C)
-    OutputMatrix("Matrix_P.txt", P)
-
-
 def GenMatrixS(R, f):
+    '''
+    通过拼音衔接矩阵R和映射f计算键位衔接矩阵S
+
+        Parameters
+        ----------
+            R   拼音衔接矩阵\n
+            f   按键-拼音映射\n
+        Returns
+        ----------
+            S   衔接矩阵\n
+'''
     S = np.zeros([26, 26], dtype=float)
     for i in range(0, 26):
         for j in range(0, 26):
             S[i][j] = R[f[i]][f[j]]
     return S
 
-
 def GetComfort(P, R, f):
+    '''
+        计算舒适度损失值
+
+        Parameters
+        ----------
+            P   线段舒适度损失矩阵\n
+            R   拼音衔接矩阵\n
+            f   按键-拼音映射\n
+        Returns
+        ----------
+            w   舒适度损失值\n
+    '''
     S = GenMatrixS(R, f)
     return np.vdot(P, S)
 
-
-'''
-    return
-        min_f   f of min w in 
-        min_w   min w value
-'''
 def Traversing(P, R):
+    '''
+        每次循环中的遍历过程
+
+        Parameters
+        ----------
+            P   线段舒适度损失矩阵\n
+            R   拼音衔接矩阵\n
+        Returns
+        ----------
+            min_f   本次循环中最小损失值对应的映射\n
+            min_w   本次循环中的最小损失值\n
+    '''
     global min_f, min_w
 
-    w = GetComfort(P, R, min_f)
-    print("\t{}\t{}\t{}".format(-1, w, min_f))
+    min_w = GetComfort(P, R, min_f)
+    print("\t{}\t{}\t{}".format(-1, min_w, min_f))
 
     for i in range(0, 3):
         lenth = len(LetterGroup[i])
@@ -73,20 +83,25 @@ def Traversing(P, R):
             if w < min_w:
                 min_w = w
                 min_f = f[:]                                    # shallow copy
-            if w/temp <= 0.99:
-               break
+            if w/temp <= 0.98:
+                break
 
         print("\t{}\t{}\t{}".format(i, min_w, min_f))
 
     return min_w, min_f
 
 
-'''
-    return
-        True    Amount of optimization keeps changing
-        False   Amount of optimization changes too slow 
-'''
 def JudgeOpt(list_w):
+    '''
+        判定循环是否继续进行
+
+        Parameters
+        ----------
+            list_w  记录了之前每次循环的最小损失值数组\n
+        Returns
+        ----------
+            Bool    True则继续优化 否则停止\n
+    '''    
     if len(list_w) < MIN_CYCLE_NUM:
         return True
 
@@ -94,3 +109,39 @@ def JudgeOpt(list_w):
         return True
 
     return False
+
+def Replace(P, R):
+    '''
+        一次循环中对可替换对的选取过程
+
+        Parameters
+        ----------
+            P   线段舒适度损失矩阵\n
+            R   拼音衔接矩阵\n
+        Returns
+        ----------
+            None
+    '''    
+    global min_f, min_w
+    min_w = GetComfort(P, R, min_f)
+    print("\t{}\t{}\t{}".format(-1, min_w, min_f))
+
+    for k in range(0,20):
+        temp = min_w
+        temp_f = min_f
+        for i in range(0, 26):
+            for j in range(0, 26): #遍历所有可交换对，寻找使w最小的交换对并执行交换
+                if ((i == 7) & (j == 25)) | ((j == 7) & (i == 25)): #v和p因为排布问题不交换
+                    continue
+                else:
+                    f = min_f[:]
+                    f[i] = min_f[j]
+                    f[j] = min_f[i]
+                    w = GetComfort(P, R, f)
+                    if w < temp:
+                        temp = w
+                        temp_f = f[:] 
+        min_f = temp_f
+        min_w = temp
+        print("\t{}\t{}\t{}".format(k, temp, temp_f))
+                
--- main_replace.py
+++ main_replace.py
@@ -1,10 +1,8 @@
-# -*- coding:utf-8 -*-
-
 import numpy as np
 import func
 import data
 
-#遍历分组优化
+#对换遍历优化
 
 if __name__ == "__main__":
     # 创建输出文件
@@ -16,11 +14,6 @@ if __name__ == "__main__":
     cycle_num = 0           # 记录循环次数
     w_list = []             # 记录每一轮循环的最小损失值
 
-    while func.JudgeOpt(w_list):                # 判定循环是否继续进行
-        min_w, min_f = func.Traversing(P, R)    # 遍历过程
-        w_list.append(min_w)
-        cycle_num += 1
-        print("{}\t{}\t{}".format(cycle_num, min_w, min_f))
-        fpo.write("No.{}\n{}\n{}\n\n".format(cycle_num, min_f, min_w))
+    func.Replace(P, R)      # 替换过程
 
     fpo.close()
